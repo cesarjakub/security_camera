235322a0fa243e862bcce4af8be07a915291f211
cesarjakub <cesar@spsejecna.cz>
2022-11-06 20:45:06 +0100

    python file zarodek

--- main.py
+++ main.py
@@ -2,3 +2,19 @@
 
 
 #code here 
+
+list = [1,2,6,4,8,5]
+
+def number_loop(obj):
+    for x in obj:
+        print(x, end=" ")
+
+def sorted_loop(obj):
+    obj = sorted(obj)
+    for x in obj:
+        print(x, end=" ")
+
+
+number_loop(list)
+print()
+sorted_loop(list)
