86a45b0ffd74641b97d3081830a3d1608639e350
cesarjakub <cesar@spsejecna.cz>
2023-01-17 17:53:49 +0100

    working cam

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 #imports here
+import cv2 
 
 
 #code here 
+vidoe = cv2.VideoCapture(0)
+while True:
+    _, okno = vidoe.read()
+    cv2.imshow("Security_cam", okno)
+    if cv2.waitKey(1) == ord('q'):
+        break
 
-list = [1,2,6,4,8,5]
-
-def number_loop(obj):
-    for x in obj:
-        print(x, end=" ")
-
-def sorted_loop(obj):
-    obj = sorted(obj)
-    for x in obj:
-        print(x, end=" ")
-
-
-number_loop(list)
-print()
-sorted_loop(list)
+vidoe.release()
+cv2.destroyAllWindows()
