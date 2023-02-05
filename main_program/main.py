46fba59b3139f9b1f0e5eba6f517b495e8cdba74
cesarjakub <cesar@spsejecna.cz>
2023-02-05 13:03:15 +0100

    feat: functions

--- main_program/main.py
+++ main_program/main.py
@@ -3,12 +3,21 @@ import cv2
 
 
 #code  
-vidoe = cv2.VideoCapture(0)
-while True:
-    _, okno = vidoe.read()
-    cv2.imshow("Security_cam", okno)
-    if cv2.waitKey(1) == ord('q'):
-        break
+def main():
+    cam()
 
-vidoe.release()
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    main()
+
+
+
+def cam():
+    vidoe = cv2.VideoCapture(0)
+    while True:
+        _, okno = vidoe.read()
+        cv2.imshow("Security_cam", okno)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    vidoe.release()
+    cv2.destroyAllWindows() 
