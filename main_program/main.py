3faeae68bccc0aa42e69a489014fa6fdb2567937
cesarjakub <cesar@spsejecna.cz>
2023-02-10 20:50:25 +0100

    feat: login page

--- main_program/main.py
+++ main_program/main.py
@@ -1,8 +1,43 @@
 #imports 
 import cv2 
-
+import tkinter as tk
+from tkinter import messagebox
 
 #code  
+class myGui:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry("300x250")
+        self.root.title("Login")
+
+        self.label = tk.Label(self.root, text="Name: ")
+        self.label.pack(padx=20, pady=10)
+
+        self.entry = tk.Entry(self.root)
+        self.entry.pack(padx=40, pady=10)
+
+        self.label1 = tk.Label(self.root, text="Password: ")
+        self.label1.pack(padx=20, pady=20)
+
+        self.entry1 = tk.Entry(self.root, show="*")
+        self.entry1.pack(padx=40, pady=2)
+
+        self.button = tk.Button(self.root, text="login", command=self.check)
+        self.button.pack(padx=20, pady=20)
+
+        self.root.mainloop()
+
+    def check(self):
+        username = self.entry.get()
+        password = self.entry1.get()
+        if username == "admin" and password == "admin":
+            print(f"{username}, {password}")
+            self.root.destroy()
+        else:
+            messagebox.showinfo(title="login error", message="heslo nebo jmeno je spatne")
+
+
+
 def detect_face(img):
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -11,21 +46,21 @@ def detect_face(img):
         cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
 
 def main():
-    cap = cv2.VideoCapture(0)
+    try:
+        if myGui():
+            cap = cv2.VideoCapture(0)
+            while True:
+                _, img = cap.read()       
+                detect_face(img)
+                cv2.imshow("Security_cam", img)    
+                if cv2.waitKey(1) == ord('q'):
+                    break
+            cap.release()
+            cv2.destroyAllWindows() 
+    except:
+        print("Something went wrong")
 
 
-    while True:
-        _, img = cap.read()
-            
-        detect_face(img)
-
-        cv2.imshow("Security_cam", img)
-            
-        if cv2.waitKey(1) == ord('q'):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows() 
 
 if __name__ == "__main__":
     main()
