8b46075e7c04ddbe11b4d01bb8ecc3195216d96a
cesarjakub <cesar@spsejecna.cz>
2023-02-12 11:53:57 +0100

    feat: small changes

--- main_program/main.py
+++ main_program/main.py
@@ -40,18 +40,18 @@ class myGui:
 
 
 
-def detect_face(img):
+def detectFace(img):
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
     for(x ,y, w, h) in faces:
-        cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
+        cv2.rectangle(img, (x,y), (x+w, y+h), (255,255,0), 2)
 
 def getVideo():
     cap = cv2.VideoCapture(0)
     while True:
         _, img = cap.read()       
-        detect_face(img)
+        detectFace(img)
         cv2.imshow("Security_cam", img)    
         if cv2.waitKey(1) == ord('q'):
             break
